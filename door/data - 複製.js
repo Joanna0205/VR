d173bb84632d66b648da0fcf1a996f6c0422a98d
Joanna0205 <joanna66168@gmail.com>
2024-12-12 10:53:04 +0800

    1212

---
+++ door/data - 複製.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "門口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 0.04016256607978974,
+        "pitch": 0.1396976834514554,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "林本源園邸(林家花園)",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
