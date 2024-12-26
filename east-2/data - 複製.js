35263486252b49337dff7e24aa4f838c32609304
Joanna0205 <joanna66168@gmail.com>
2024-12-26 11:11:27 +0800

    Create data - 複製.js

---
+++ east-2/data - 複製.js
@@ -0,0 +1,247 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "香玉簃",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.5145864297409304,
+        "pitch": 0.16898718117871425,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10812741497356448,
+          "pitch": 0.07895699517247756,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4383495370989436,
+          "pitch": 0.34262610212299904,
+          "title": "香玉簃",
+          "text": "是一種閣樓邊的小屋，也是一個中繼性質的景點。它的主景並非建築本身，而是前方的花圃"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "來青閣",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 0.8952759122420133,
+        "pitch": -0.03786032634534919,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3507922810091202,
+          "pitch": 0.021175120968766947,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.9302785456036,
+          "pitch": 0.014984535783606034,
+          "rotation": 5.497787143782138,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6324362183284968,
+          "pitch": -0.030995965362045297,
+          "title": "來青閣",
+          "text": "昔日為貴賓下榻之處。閣的兩側設有庭院，以花牆做區隔"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "方鑑齋",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -0.7101616802952506,
+        "pitch": 0.14076071891929942,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.194084292093482,
+          "pitch": 0.0735842041875241,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.7204389606888189,
+          "pitch": 0.5263888680889526,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.955617741300193,
+          "pitch": 0.18919388196897557,
+          "title": "方鑑齋",
+          "text": "為昔日讀書之所，齋前鑿池，池中設有戲台"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "汲古書屋",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -0.07433545006362685,
+        "pitch": 0.0985307351071949,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0009785224379292856,
+          "pitch": -0.14168588232054447,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -2.4578862138535307,
+          "pitch": 0.7174232256206405,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.14005302473219317,
+          "pitch": 0.2685936159716462,
+          "title": "汲古書屋",
+          "text": "主要功能是藏書與讀書"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "汲古書屋後",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -0.35587993464366896,
+        "pitch": 0.5083522351616949,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.402473641305349,
+          "pitch": 0.5713266779082566,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "林本源園邸(林家花園)",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
